d86a0b7eb2c20756f1fe258e2c6859b74117ad8c
MichiY3 <michiyoy@hotmail.com>
2020-11-10 22:12:36 +0900

    完成

--- swiper2/sample.js
+++ swiper2/sample.js
@@ -1,7 +1,7 @@
 var mySwiper = new Swiper ('.swiper-container', {
   loop: true,
   slidesPerView: 2,
-  spaceBetween: 10,
+  spaceBetween: 15,
   centeredSlides : true,
   pagination: '.swiper-pagination',
   nextButton: '.swiper-button-next',
